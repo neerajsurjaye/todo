63b40709106c1d5b2f417a76ac13fc60614f80ee
Neeraj Surjaye <neerajsurjaye@gmail.com>
2020-12-12 12:33:06 +0530

    non gui part

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("ran");
+(()=>{"use strict";let o=(()=>{let o=[],t=o=>({property:o,todos:{}});return o.push(t("default")),{add_project:e=>{o.push(t(e))},add_todo:(t,e,d,r,p)=>{for(let e=0;e<o.length;e++)o[e].property==t&&console.log(o[e].property)}}})();o.add_project("second",234,324,234,543),o.add_todo("second")})();
